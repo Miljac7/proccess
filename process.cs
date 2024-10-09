b09cab3de252576091ba1fba9cef058169a00ff1
Miljac7 <152974534+Miljac7@users.noreply.github.com>
2024-10-09 09:03:28 +0200

    Add files via upload

---
+++ process.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_aplikacije
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+            ProcessStartInfo startInfo = new ProcessStartInfo("IExplore.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("Excel.exe");
+            ProcessStartInfo startInfo = new ProcessStartInfo("Excel.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+        }
+
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("Word.exe");
+            ProcessStartInfo startInfo = new ProcessStartInfo("Word.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("Notepad.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+        }
+    }
+}
